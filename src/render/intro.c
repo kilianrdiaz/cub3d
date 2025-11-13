1c2340ae419b3c63a5872beac1c5d33c2c1370a9
Alejandro Hernández <alejandro1998hernandez@gmail.com>
2025-11-13 17:06:06 +0100

    Refactor intro handling: update close_intro logic to trigger after 60 seconds; modify read_intro to display "PRESS ANY BUTTON TO PLAY" conditionally.

--- src/render/intro.c
+++ src/render/intro.c
@@ -15,11 +15,12 @@
 static int	close_intro(int keycode, t_game *g)
 {
 	(void)keycode;
-	if (g->render_state == INTRO)
+	if (g->render_state == INTRO && g->timer > 60)
 	{
 		g->render_state = LOAD_LEVEL;
 		ft_bzero(&g->keys, sizeof(t_keys));
 		clean_screen(g);
+		g->timer = 0;
 		mlx_hook(g->win, 2, 1L << 0, key_press, g);
 		mlx_hook(g->win, 3, 1L << 1, key_release, g);
 	}
@@ -28,24 +29,29 @@ static int	close_intro(int keycode, t_game *g)
 
 static int	read_intro(t_game *g)
 {
-	int		fd;
-	char	*line;
-	t_pos	p;
+	int			fd;
+	char		*line;
+	t_coords	coords;
 
 	fd = open("./textures/intro.txt", O_RDONLY);
 	if (fd == -1)
 		return (0);
-	p.x = 160;
-	p.y = 60;
+	coords.x = 160;
+	coords.y = 60;
 	line = get_next_line(fd);
 	g->font.scale = 1.2;
 	while (line)
 	{
-		render_text(g, line, (t_coords){p.x, p.y});
-		p.y += g->font.char_h * 0.8 + 10;
+		render_text(g, line, coords);
+		coords.y += g->font.char_h * 0.8 + 10;
 		free(line);
 		line = get_next_line(fd);
 	}
+	coords.y += g->font.char_h * 0.8 + 10;
+	coords.x = WIDTH / 2 - ((int)ft_strlen("PRESS ANY BUTTON TO PLAY")
+			* g->font.char_w * g->font.scale) / 2;
+	if (g->timer > 60 && g->timer % 60 < 30)
+		render_text(g, "PRESS ANY BUTTON TO PLAY", coords);
 	close(fd);
 	return (1);
 }
@@ -54,11 +60,11 @@ void	show_intro(t_game *g)
 {
 	t_tex	intro;
 
-	g->render_state = INTRO;
 	load_texture(g, &intro, "./textures/intro.xpm");
 	draw_fullscreen_image(g, intro);
 	read_intro(g);
 	mlx_put_image_to_window(g->mlx, g->win, g->img, 0, 0);
+	g->timer++;
 	mlx_key_hook(g->win, close_intro, g);
 	if (intro.img)
 		mlx_destroy_image(g->mlx, intro.img);
